87131537d9b1c2ecede125eb129bf011597df497
Carlos <pirumpi@gmail.com>
2015-01-20 12:22:36 -0700

    Updating publicIP DNS

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 var pc = require('./lib');
 
-// pc.get(['cpu', 'memory', 'publicIP', 'share']).then(function(list){
-//     console.log(list);
-// }, function(err){
-//     console.log('ERR',err)
-// })
-
-console.log(pc.tmpDir());
+pc.get(['cpu', 'memory', 'publicIP', 'share']).then(function(list){
+    console.log(list);
+}, function(err){
+    console.log('ERR',err)
+ })
